32750a4649f2b0a6dce67e99ca75317833aa7ff3
MohammadRezaHosseinian <81797831+MohammadRezaHosseinian@users.noreply.github.com>
2021-06-14 16:38:12 +0430

    Add files via upload

---
+++ 8/Client/src/client/Client.java
@@ -0,0 +1,16 @@
+
+package client;
+
+/**
+ *
+ * @author mohammadreza
+ */
+public class Client {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String args[]) {
+		ClientApp client = new ClientApp("127.0.0.1", 5000);
+	}
+}
---
+++ 8/Client/src/client/ClientApp.java
@@ -0,0 +1,85 @@
+
+
+package client;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author mohammadreza
+ */
+public class ClientApp {
+	// A Java program for a Client
+
+
+
+
+	// initialize socket and input output streams
+	private Socket socket		 = null;
+	private DataInputStream input = null;
+	private DataOutputStream out	 = null;
+
+	// constructor to put ip address and port
+	public ClientApp(String address, int port)
+	{
+		// establish a connection
+		try
+		{
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+
+			// takes input from terminal
+			input = new DataInputStream(System.in);
+
+			// sends output to the socket
+			out = new DataOutputStream(socket.getOutputStream());
+		}
+		catch(UnknownHostException u)
+		{
+			System.out.println(u);
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+
+		// string to read message from input
+		String line = "";
+
+		// keep reading until "Over" is input
+		while (!line.equals("Over"))
+		{
+			try
+			{
+				line = input.readLine();
+				out.writeUTF(line);
+			}
+			catch(IOException i)
+			{
+				System.out.println(i);
+			}
+		}
+
+		// close the connection
+		try
+		{
+			input.close();
+			out.close();
+			socket.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+	}
+
+	
+}
